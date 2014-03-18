391d3bd1b45afefd945c4742269886e8a5ececc3
Nina Dang <ninadangt@gmail.com>
2014-03-18 16:33:23 -0700

    Dynamic array, fonts, footer.

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(function() {
+	var viewportheight = $(window).height();
+	$('#slide1').height(viewportheight);
+
+	var words = new Array('eats', 'jams', 'wanders');
+	var i = 0;
+	setInterval(function() {
+		$('.verb').empty().append(words[i]);
+		if(i < words.length) {
+			i++;
+		} else {
+			i = 0;
+		}
+	}, 2500);
+});
